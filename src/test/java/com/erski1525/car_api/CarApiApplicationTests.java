cf88eeee7e8a5f33efa6bd39d83bdab3c9c3bacd
Eryk Ciesielski <eryk@192.168.0.106>
2024-06-25 16:14:10 +0200

    initial

---
+++ src/test/java/com/erski1525/car_api/CarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erski1525.car_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
